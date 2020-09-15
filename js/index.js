3201ca6817e81d6dcbdf9e920838822307777c7d
Zach Benjamin Thomas <s0452529@sms.ed.ac.uk>
2020-09-15 12:08:41 +0100

    Adding filter jquery code

--- js/index.js
+++ js/index.js
@@ -11,3 +11,32 @@
 		this.firstElementChild.firstElementChild.style.color = getComputedStyle(document.documentElement).getPropertyValue("--secondary-bg-color");
 	})
 });
+
+// Filters for Product Content
+
+$(".filter").click(function() {
+
+	// Get the target filter from the clicked item
+    var target = $(this).attr("target-filter");
+
+    // Mark the clicked item as an active filter
+    $(this).toggleClass("active-filter");
+
+    // Build up selector based on all currently selected filters.
+    var targetString = "";
+    var noActiveFilters = $(".active-filter").length;
+
+    // for(var i=0;i<=noActiveFilters;i++) {
+    // 	targetString = targetString + "." + $(".active-filter").get(i).attr("target-filter");
+    // }
+    $(".active-filter").each(function() {
+    	targetString += "." + $(this).attr("target-filter");
+    })
+
+
+   	// Make all cards disappear
+    $(".product-card").addClass("filtered");
+
+    // Allow items matching all filters to reappear.
+    $(targetString).removeClass("filtered"); // Consider animation here
+    })

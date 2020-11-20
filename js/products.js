14ea406d8fcbb9ae3338c4d17349cc0d74e33f0b
Zach Benjamin Thomas <s0452529@sms.ed.ac.uk>
2020-11-20 16:18:38 +0000

    Fix sticky panel code

--- js/products.js
+++ js/products.js
@@ -1,47 +1,47 @@
-// Sticky heading for products page
+// // Sticky heading for products page
 
-// Get the header
-var header = $("#product-filters");
+// // Get the header
+// var header = $("#product-filters");
 
-// Get the offset position of the navbar
-var headerOffset = header.offset().top;
+// // Get the offset position of the navbar
+// var headerOffset = header.offset().top;
 
-// Get the original padding of the product card section
-var originalPadding = parseInt($("#product-cards").css("padding-top"));
-var filtersHeight = parseInt($("#product-filters").outerHeight());
+// // Get the original padding of the product card section
+// var originalPadding = parseInt($("#product-cards").css("padding-top"));
+// var filtersHeight = parseInt($("#product-filters").outerHeight());
 
-// Get the original menu position
-var navBarHeight = $("#title").outerHeight();
+// // Get the original menu position
+// var navBarHeight = $("#title").outerHeight();
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function(){ 
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function(){ 
 
-	// Correction for expanded navbar
-	if(navBarHeight !== $("#title").outerHeight()){
-		navBarHeight = $("#title").outerHeight();
-		headerOffset = header.offset().top;
-	}
+// 	// Correction for expanded navbar
+// 	if(navBarHeight !== $("#title").outerHeight()){
+// 		navBarHeight = $("#title").outerHeight();
+// 		headerOffset = header.offset().top;
+// 	}
 
-	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-	if (window.pageYOffset > headerOffset) {
-		if(!header.hasClass("sticky")){
-			header.addClass("sticky");
-			$("#product-cards").css("padding-top", filtersHeight + originalPadding);
-		}
-	} else {
-		if(header.hasClass("sticky")){
-			header.removeClass("sticky");
-			$("#product-cards").css("padding-top", originalPadding);
-		}
-	}
+// 	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// 	if (window.pageYOffset > headerOffset) {
+// 		if(!header.hasClass("sticky")){
+// 			header.addClass("sticky");
+// 			$("#product-cards").css("padding-top", filtersHeight + originalPadding);
+// 		}
+// 	} else {
+// 		if(header.hasClass("sticky")){
+// 			header.removeClass("sticky");
+// 			$("#product-cards").css("padding-top", originalPadding);
+// 		}
+// 	}
 
-};
+// };
 
-// Corrections when window is resized
-window.onresize = function(){
+// // Corrections when window is resized
+// window.onresize = function(){
 
-	originalPadding = parseInt($("#product-cards").css("padding-top"));
-	filtersHeight = parseInt($("#product-filters").outerHeight());
-}
+// 	originalPadding = parseInt($("#product-cards").css("padding-top"));
+// 	filtersHeight = parseInt($("#product-filters").outerHeight());
+// }
 
 

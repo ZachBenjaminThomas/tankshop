c7f2f11ae27e537033413af2c2708b686860fa83
Zach Benjamin Thomas <s0452529@sms.ed.ac.uk>
2020-11-23 15:01:57 +0000

    Screen scroll behaviour

--- js/filters.js
+++ js/filters.js
@@ -2,11 +2,31 @@
 
 $(".filter").click(function() {
 
+    // Record starting screen positions
+    startScreenPosition = $(document).scrollTop();
+
 	// Get the target filter from the clicked item
     var target = $(this).attr("target-filter");
 
+    // Get the filter type
+    var type = $(this).attr("filter-type");
+
     // Mark the clicked item as an active filter
     $(this).toggleClass("active-filter");
+
+    // Check for category specific sections
+    if($("#"+target).length > 0) {
+
+        // Remove all category specific sections first
+        $(".category-specific[filter-type='"+type+"']").addClass("filtered");
+
+        // Display any category specific sections on the product page
+        if($(this).hasClass("active-filter")){
+            $("#"+target).removeClass("filtered");  
+        }
+    } else {
+        $(".category-specific[filter-type='"+type+"']").addClass("filtered");
+    }
 
     // Deal with product type filters
     if($(this).hasClass("product-filter")){
@@ -28,7 +48,6 @@ $(".filter").click(function() {
     var targetString = "";
     var noActiveFilters = $(".active-filter").length;
 
-    // 
     $(".active-filter").each(function() {
     	targetString += "." + $(this).attr("target-filter");
     })
@@ -42,12 +61,28 @@ $(".filter").click(function() {
     	// Allow items matching all filters to reappear.
     	$(targetString).removeClass("filtered"); // Consider animation here
 
+        // If no items are visible display a "Not found" message.
+        if($("#product-cards .card").not(".filtered").length === 0) {
+            $("#notfoundmessage").removeClass("filtered");
+        } else {
+            $("#notfoundmessage").addClass("filtered");
+        }
+
     } else {
 
     	// Make all cards reappear if no filters present
     	$(".product-card").removeClass("filtered");
 
+        // Remove the not found message too    
+        $("#notfoundmessage").addClass("filtered");
+
     }
+
+    // Return to top of product card section
+    if(startScreenPosition >= $("#title").height()){ 
+        window.scrollTo(0,$("#title").height());
+    }
+    
  })
 
 
